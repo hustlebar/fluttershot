02103a902c08206f8d08615042cad6f05f6ff76b
Thamizharasu S <zarub2k@gmail.com>
2018-07-05 19:11:33 +0530

    Chat work started

---
+++ lib/friendlychat/chatscreen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Friendly Chat'),
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _controller,
+              onSubmitted: _onSubmit,
+              decoration: const InputDecoration.collapsed(hintText: 'Send a message'),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 5.0),
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () => _onSubmit(_controller.text),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onSubmit(String value) {
+    print('Value $value');
+    _controller.clear();
+  }
+}
--- lib/friendlychat/friendlychat.dart
+++ lib/friendlychat/friendlychat.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
+import 'chatscreen.dart';
 
 class FriendlyChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Friendly Chat',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Friendly Chat'),
-        ),
-      ),
+      home: ChatScreen()
     );
   }
 }
